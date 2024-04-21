496f3f40a9d91c43070b9aae16779ae34081959e
Surendra Kumar <asurendrakumarpatel@gmail.com>
2024-04-21 17:01:10 +0530

    code updated

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,23 @@
 import NextAuth, { Session } from "next-auth";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
- 
+
 const authConfig = {
-    pages:{
-        signIn:"/login",
+    pages: {
+        signIn: "/login",
     },
-    providers:[],
-    callbacks:{
-        async authorized({auth, request}:{auth: Session | null; request:NextRequest}){
+    providers: [],
+    callbacks: {
+        async authorized({ auth, request }: { auth: Session | null; request: NextRequest }) {
             const user = auth?.user;
             const chatPath = request.nextUrl.pathname.startsWith("/chat");
             const authPath = request.nextUrl.pathname.startsWith("/login") || request.nextUrl.pathname.startsWith("/signup");
 
-            if(!user && chatPath){
+            if (!user && chatPath) {
                 return false;
             }
-
-            if(user && authPath){
-                return Response.redirect(new URL("/chat", request.nextUrl));
+            if (user && authPath) {
+                return NextResponse.redirect(new URL("/chat", request.nextUrl));
             }
             return true;
         }
