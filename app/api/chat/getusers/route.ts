12bf5d69af461eeed84eb9d13b2deff1ea7e5ba7
Surendra Kumar <asurendrakumarpatel@gmail.com>
2024-04-21 16:10:50 +0530

    ready for deploy

--- app/api/chat/getusers/route.ts
+++ app/api/chat/getusers/route.ts
@@ -1,12 +1,14 @@
 import { auth } from "@/auth"
 import connectDB from "@/lib/db";
 import { User, UserDocument } from "@/models/user.model";
+import { unstable_noStore } from "next/cache";
 import { NextResponse } from "next/server";
 
 export const GET = async () => {
+    unstable_noStore();
     try {
         const authUser = await auth();
-        if(!authUser) return;
+        if(!authUser) return ;
         await connectDB();
         
         const users:UserDocument[] = await User.find({_id:{$ne:authUser?.user?._id}});
@@ -16,6 +18,5 @@ export const GET = async () => {
         return NextResponse.json(users);
     } catch (error) {
         console.log(error);
-        
     }
 }
